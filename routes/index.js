bc06b8dc85a5340ccde08d63e836865799a86522
shisaq <shisaq@gmail.com>
2017-02-13 18:02:51 -0500

    play: customize info to my own; add `id` to `/users` route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: '白水佳境',
+        age: 28
+    });
 });
 
 module.exports = router;
